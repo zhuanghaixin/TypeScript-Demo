3ee7c70b34a42f1090be2d3f8b514d6771a1a1c0
zhuanghaixin <649308293@qq.com>
2020-06-02 23:59:56 +0800

    refactor(crowller.ts): 函数解藕

--- typescript-spider/src/crowller.ts
+++ typescript-spider/src/crowller.ts
@@ -1,10 +1,12 @@
 import superagent from 'superagent'
 import cheerio from 'cheerio'
-interface Product{
+
+interface Product {
     productName: string,
-    price:number
+    price: number
 }
-class Crowller{
+
+class Crowller {
     // private secret='secretKey'
     // private url = `http://www.dell-lee.com/typescript/demo.html?secret=${this.secret}`;
     //     // private url = "http://www.thenewstep.cn/msonline/"
@@ -12,42 +14,57 @@ class Crowller{
     // private url = "http://www.dell-lee.com"
     private url = "http://www.thenewstep.cn/"
 
-    getProductInfo(html:string){
-        const $=cheerio.load(html)
-        const courseItems=$('.product')
+    getProductInfo(html: string) {
+        const $ = cheerio.load(html)
+        const courseItems = $('.product')
         console.log(courseItems.length)
-        const productInfos:Product[]=[]
-        courseItems.map((index,element) =>{
-            const elementProductName=$(element).find('.product-name')
-            const productName=elementProductName.text()
-            const elementPrice=$(element).find('.product-price')
-            const price=parseInt(elementPrice.text())
-            console.log(productName,price)
+        const productInfos: Product[] = []
+        courseItems.map((index, element) => {
+            const elementProductName = $(element).find('.product-name')
+            const productName = elementProductName.text()
+            const elementPrice = $(element).find('.product-price')
+            const price = parseInt(elementPrice.text())
+            console.log(productName, price)
             productInfos.push({
-             productName,price
+                productName, price
             })
         })
         return {
-            time:new Date().getTime(),
-            data:productInfos
+            time: new Date().getTime(),
+            data: productInfos
         }
 
     }
+
+// async getRawHtml(){
+//     //因为返回值是Promise
+//     const result=await superagent.get(this.url)
+//     console.log(result.text)
+//     this.getProductInfo(result.text)
+// }
+// constructor(){
+//     this.getRawHtml()
+//     console.log('constructor')
+// }
+
+    async getRawHtml() {
+        //因为返回值是Promise
+        const result = await superagent.get(this.url)
+        return result.text
+    }
+
     //每个函数只执行一个功能
-    async initSpiderProcess(){
-         const html= await  this.getRawHtml()
+    async initSpiderProcess() {
+        const html = await this.getRawHtml()
         console.log(html)
         const courseResult = this.getProductInfo(html)
     }
-    async getRawHtml(){
-        //因为返回值是Promise
-        const result=await superagent.get(this.url)
-        return result.text
-    }
-    constructor(){
-      this.initSpiderProcess()
+
+    constructor() {
+        this.initSpiderProcess()
     }
 }
+
 const croller = new Crowller()
 
 
