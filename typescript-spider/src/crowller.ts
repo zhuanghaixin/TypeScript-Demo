c8d14a586f86fe8fce2d55269947c6570c4d6a60
zhuanghaixin <649308293@qq.com>
2020-06-03 01:19:23 +0800

    refactor(crowller.ts): 爬取数据设计和存储 生成data/product.json

--- typescript-spider/src/crowller.ts
+++ typescript-spider/src/crowller.ts
@@ -1,10 +1,42 @@
 import superagent from 'superagent'
 import cheerio from 'cheerio'
+import fs from 'fs'
+import path from 'path'
 
 interface Product {
     productName: string,
     price: number
 }
+
+interface ProductResult {
+    time: number,
+    data: Product[]
+}
+
+// //json结构
+// {
+//     1591113666686: [
+//     { productName: 'html', price: 18 },
+//     { productName: 'css', price: 18 },
+//     { productName: 'javascript', price: 18 },
+//     { productName: 'nodejs', price: 48 },
+//     { productName: 'react', price: 58 },
+//     { productName: 'vue', price: 68 }
+// ],
+//     1592234434443: [
+//     { productName: 'html', price: 18 },
+//     { productName: 'css', price: 18 },
+//     { productName: 'javascript', price: 18 },
+//     { productName: 'nodejs', price: 48 },
+//     { productName: 'react', price: 58 },
+//     { productName: 'vue', price: 68 }
+// ],
+// }
+
+interface Content {
+    [propName: number]: Product[]
+}
+
 
 class Crowller {
     // private secret='secretKey'
@@ -53,11 +85,41 @@ class Crowller {
         return result.text
     }
 
+    //将传的数据生成json文件
+    generateJsonContent(productInfo: ProductResult) {
+        console.log(444444)
+        console.log(productInfo)
+        const filePath = path.resolve(__dirname, '../data/product.json')
+        console.log(filePath)
+        //如果文件存在，则读出来，如果文件不存在，则创建文件的初始内容
+        let fileContent: Content = {}
+        console.log("fs.existsSync(filePath)")
+        console.log(fs.existsSync(filePath))
+        if (fs.existsSync(filePath)) {
+            //读取内容
+            console.log(111111)
+            fileContent = JSON.parse(fs.readFileSync(filePath, 'utf-8'))
+        }
+        console.log(111)
+        fileContent[productInfo.time] = productInfo.data
+        console.log(22222)
+
+        return fileContent
+
+
+    }
+
     //每个函数只执行一个功能
     async initSpiderProcess() {
         const html = await this.getRawHtml()
         console.log(html)
-        const courseResult = this.getProductInfo(html)
+        const productInfo = this.getProductInfo(html)
+        console.log(productInfo)
+        //生成json
+        const fileContent = this.generateJsonContent(productInfo)
+        //写文件
+        const filePath = path.resolve(__dirname, '../data/product.json')
+        fs.writeFileSync(filePath, JSON.stringify(fileContent))
     }
 
     constructor() {
