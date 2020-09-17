285dbbc704a184429402e2f35198b2d0e46270d6
zhuanghaixin <649308293@qq.com>
2020-09-17 10:11:39 +0800

    feat(Udemy-TypeScript):Chapter10完成namespaces✅,学习TypeScript的namespaces命名空间

---
+++ Udemy-TypeScript/Chapter10/namespaces/src/app.ts
@@ -0,0 +1,16 @@
+
+/// <reference path="components/project-input.ts" />
+/// <reference path="components/project-list.ts" />
+
+
+
+namespace App {
+
+
+    new ProjectInput()
+
+//ProjectInput Class
+    new ProjectList('active')
+    new ProjectList('finished')
+}
+
